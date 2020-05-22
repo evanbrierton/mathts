bcf57166d4470d140c4bff8a2610e4736b11179e
Nikita Skobelevs <nikita.skobelevs@gmail.com>
2020-05-22 14:28:58 +0100

    Added point functionality
    Signed-off-by: Nikita Skobelevs <nikita.skobelevs@gmail.com>

--- src/geometry/Point.ts
+++ src/geometry/Point.ts
@@ -13,6 +13,46 @@ class Point {
     const { x: x2, y: y2 } = other;
     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
   }
+
+  add(other: Point) {
+    return new Point(this.x + other.x, this.y + other.y);
+  }
+
+  sub(other: Point) {
+    return new Point(this.x - other.x, this.y - other.y);
+  }
+
+  mult(scalar: number) {
+    return new Point(this.x * scalar, this.y * scalar);
+  }
+
+  div(scalar: number) {
+    return new Point(this.x / scalar, this.y / scalar);
+  }
+
+  dot(other: Point) {
+    return this.x * other.x + this.y * other.y;
+  }
+
+  static fromAngle(theta: number) {
+    return new Point(Math.cos(theta), Math.sin(theta));
+  }
+
+  get magSq() {
+    return this.x ** 2 + this.y ** 2;
+  }
+
+  get mag() {
+    return Math.sqrt(this.magSq);
+  }
+
+  get heading() {
+    return Math.atan2(this.y, this.x);
+  }
+
+  get normalized() {
+    return Point.fromAngle(this.heading);
+  }
 }
 
 export default Point;
