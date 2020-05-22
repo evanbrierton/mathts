549c9502026cce8f4b6675e5926353b509be6213
Evan Brierton <evanbrierton@icloud.com>
2020-05-22 15:31:07 +0100

    Implement Matrix.generate and add stock generators

--- src/matrices/Matrix.ts
+++ src/matrices/Matrix.ts
@@ -15,6 +15,27 @@ class Matrix extends Array<Array<Number>> {
     this.columns = rows[0].length;
     this.square = rows.length === rows[0].length;
   }
+
+  static generate(
+    n: number, m: number, callback: (i: number, j: number) => number,
+  ) {
+    return new Matrix(
+      Array.from({ length: n })
+        .map((_row, i) => (Array.from({ length: m }).map((_entry, j) => callback(i, j)))),
+    );
+  }
+
+  static identity(n: number) {
+    return Matrix.generate(n, n, (i, j) => (i === j ? 1 : 0));
+  }
+
+  static zero(n: number) {
+    return Matrix.generate(n, n, () => 0);
+  }
+
+  static random(n: number, m: number, min: number, max: number) {
+    return Matrix.generate(n, m, () => Math.floor(Math.random() * (max - min) + min));
+  }
 }
 
 export default Matrix;
